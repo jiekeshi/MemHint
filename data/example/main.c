fee1e74c4ea01c5857dd16da66fc3bbdb3b2f5dd
jiekeshi <jiekeshi125@gmail.com>
2026-01-12 13:42:52 +0800

    Comment out unused function and its calls in main.c

--- data/example/main.c
+++ data/example/main.c
@@ -1,25 +1,43 @@
+// #include <stdlib.h>
+
+// int foo(int n) {
+//     int *p = NULL;
+
+//     if (n > 0) {
+//         p = (int *)malloc(sizeof(int));
+//         *p = 42;
+//     }
+
+//     if (n < 0) {
+//         free(p);
+//     }
+
+//     if (n > 10) {
+//         return *p;   // ❌ use-after-free
+//     }
+
+//     return 0;
+// }
+
+// int main() {
+//     foo(15);
+//     return 0;
+// }
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int foo(int n) {
-    int *p = NULL;
-
-    if (n > 0) {
-        p = (int *)malloc(sizeof(int));
-        *p = 42;
+char* allocate_memory() {
+    char *str = (char*)malloc(100 * sizeof(char));
+    if (str != NULL) {
+        strcpy(str, "Hello, World!");
     }
-
-    if (n < 0) {
-        free(p);
-    }
-
-    if (n > 10) {
-        return *p;   // ❌ use-after-free
-    }
-
-    return 0;
+    return str;
 }
 
 int main() {
-    foo(15);
+    char *str = allocate_memory();
+    printf("%s\n", str);
     return 0;
 }
