5b826da2d78dd33a207b04696335c1dd3fa11f7c
huanghuihui0904 <625173@qq.com>
2026-01-17 22:45:15 +0800

    add new proj

--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 export GOOGLE_APPLICATION_CREDENTIALS=/home/huihuihuang/Hint/huihui-472807-b8069b88637c.json
 export PATH=/home/huihuihuang/Hint/codeql:$PATH
-# PROJECT_PATH=/home/huihuihuang/Hint/bugscpp/cpp_peglib/buggy-1  
-# OUTPUT_PATH=/home/huihuihuang/Hint/output/bugscpp/cpp_peglib/buggy-1/output
-PROJECT_PATH=/home/huihuihuang/Hint/defect4c_clone_proj/njs
-OUTPUT_PATH=/home/huihuihuang/Hint/output/defect4c_clone_proj/njs/output
+CPP_QUERIES_DIR="/home/huihuihuang/Hint/codeql/qlpacks/codeql/cpp-queries"
+
+##########
+# Redis #
+##########
+# PROJECT_PATH=/home/huihuihuang/Hint/cloned_proj/redis
+# OUTPUT_PATH=/home/huihuihuang/Hint/output/cloned_proj/redis/output_single_source
+# mkdir -p $OUTPUT_PATH
+# python main.py --debug --project $PROJECT_PATH --output $OUTPUT_PATH \
+#     --cpp-queries-dir $CPP_QUERIES_DIR \
+#     --single-source "/home/huihuihuang/Hint/cloned_proj/redis/src/replication.c" "/home/huihuihuang/Hint/cloned_proj/redis/src/redis-benchmark.c"
+
+##########
+# Htop #
+##########
+PROJECT_PATH=/home/huihuihuang/Hint/cloned_proj/htop
+OUTPUT_PATH=/home/huihuihuang/Hint/output/cloned_proj/htop/output_single_source
 mkdir -p $OUTPUT_PATH
-# python main.py --project $PROJECT_PATH --output $OUTPUT_PATH --merge
-python main.py --project $PROJECT_PATH --output $OUTPUT_PATH --single-source "/home/huihuihuang/Hint/defect4c_clone_proj/njs/src/njs_function.c" --cpp-queries-dir "/home/huihuihuang/Hint/codeql/qlpacks/codeql/cpp-queries"
-# python main.py --project $PROJECT_PATH --output $OUTPUT_PATH --single-source "/home/huihuihuang/Hint/defect4c_clone_proj/njs/src/njs_function.c" --cpp-queries-dir "/home/huihuihuang/Hint/codeql/qlpacks/codeql/cpp-queries" --no-reuse-db
+python main.py --debug --project $PROJECT_PATH --output $OUTPUT_PATH \
+    --cpp-queries-dir $CPP_QUERIES_DIR \
+    --single-source "/home/huihuihuang/Hint/cloned_proj/htop/Settings.c" "/home/huihuihuang/Hint/cloned_proj/htop/XUtils.c"
