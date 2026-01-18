c094e8e58a80075c30a2c7c8500a1a32bb64c903
huanghuihui0904 <625173@qq.com>
2026-01-18 15:47:59 +0800

    add new proj

--- run.sh
+++ run.sh
@@ -15,9 +15,19 @@ CPP_QUERIES_DIR="/home/huihuihuang/Hint/codeql/qlpacks/codeql/cpp-queries"
 ##########
 # Htop #
 ##########
-PROJECT_PATH=/home/huihuihuang/Hint/cloned_proj/htop
-OUTPUT_PATH=/home/huihuihuang/Hint/output/cloned_proj/htop/output_single_source
+# PROJECT_PATH=/home/huihuihuang/Hint/cloned_proj/htop
+# OUTPUT_PATH=/home/huihuihuang/Hint/output/cloned_proj/htop/output_single_source
+# mkdir -p $OUTPUT_PATH
+# python main.py --debug --project $PROJECT_PATH --output $OUTPUT_PATH \
+#     --cpp-queries-dir $CPP_QUERIES_DIR \
+#     --single-source "/home/huihuihuang/Hint/cloned_proj/htop/Settings.c" "/home/huihuihuang/Hint/cloned_proj/htop/XUtils.c"
+
+##########
+# tmux_pr3937 #
+##########
+PROJECT_PATH=/home/huihuihuang/Hint/cloned_proj/tmux_pr3937
+OUTPUT_PATH=/home/huihuihuang/Hint/output/cloned_proj/tmux_pr3937/output_single_source
 mkdir -p $OUTPUT_PATH
 python main.py --debug --project $PROJECT_PATH --output $OUTPUT_PATH \
     --cpp-queries-dir $CPP_QUERIES_DIR \
-    --single-source "/home/huihuihuang/Hint/cloned_proj/htop/Settings.c" "/home/huihuihuang/Hint/cloned_proj/htop/XUtils.c"
+    --single-source "/home/huihuihuang/Hint/cloned_proj/tmux_pr3937/arguments.c"
